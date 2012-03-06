a71037db201233ea59c4673ba9e2e94055e8f7f2
Luciano Coelho <coelho@ti.com>
2012-03-06 11:46:49 +0200

    hostapd: only updated already-set beacons when processing OLBC
    When hostapd is about to start an AP using HT40, it starts a scan to
    check whether it is allowed or not.  If OLBC is detected before the
    scan has completed, it sets the beacons prematurely.
    
    To fix this, instead of setting all beacons when OLBC is detected,
    only update the ones that have already been started.
    
    Signed-off-by: Luciano Coelho <coelho@ti.com>

--- src/ap/ap_list.c
+++ src/ap/ap_list.c
@@ -326,7 +326,7 @@ void ap_list_process_beacon(struct hostapd_iface *iface,
 #endif /* CONFIG_IEEE80211N */
 
 	if (set_beacon)
-		ieee802_11_set_beacons(iface);
+		ieee802_11_update_beacons(iface);
 }
 
 
@@ -381,7 +381,7 @@ static void ap_list_timer(void *eloop_ctx, void *timeout_ctx)
 	}
 
 	if (set_beacon)
-		ieee802_11_set_beacons(iface);
+		ieee802_11_update_beacons(iface);
 }
 
 
