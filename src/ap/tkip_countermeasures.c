b686b9b88bcc18aa7f8cef5c57593f07ca03961f
Eliad Peller <eliad@wizery.com>
2012-04-22 18:18:15 +0300

    add set/cancel_priority to ap mode as well

--- src/ap/tkip_countermeasures.c
+++ src/ap/tkip_countermeasures.c
@@ -49,6 +49,7 @@ static void ieee80211_tkip_countermeasures_start(struct hostapd_data *hapd)
 				       WLAN_REASON_MICHAEL_MIC_FAILURE);
 		ap_sta_set_authorized(hapd, sta, 0);
 		sta->flags &= ~(WLAN_STA_AUTH | WLAN_STA_ASSOC);
+		ap_sta_cancel_priority(hapd, sta);
 		hostapd_drv_sta_remove(hapd, sta->addr);
 	}
 }
